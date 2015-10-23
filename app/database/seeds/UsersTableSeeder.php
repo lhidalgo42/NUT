3784416f1608954acec568b6c37df6a305a1339c
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2015-10-23 15:07:55 -0300

    grant access

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -8,19 +8,22 @@ class UsersTableSeeder extends Seeder {
             'email' => 'eortega@inia.cl',
             'password' => '1234',
             'img' => 'avatar_2x.png',
-            'name' => 'Eduardo Ortega'
+            'name' => 'Eduardo Ortega',
+            'access' => 1
         ]);
         User::create([
             'email' => 'gjury@psiconutricion.cl',
             'password' => '1234',
             'img' => 'avatar_2x.png',
-            'name' => 'Gloria Jury'
+            'name' => 'Gloria Jury',
+            'access' => 1
         ]);
         User::create([
             'email' => 'leontuna@gmail.com',
             'password' => '1234',
             'img' => 'avatar_2x.png',
-            'name' => 'Leonardo Hidalgo'
+            'name' => 'Leonardo Hidalgo',
+            'access' => 1
         ]);
 	}
 
