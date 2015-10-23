8f0955380bb30411adce458e6ab4fedf4a06741e
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2015-10-23 15:09:33 -0300

    fix linux

--- app/database/migrations/2015_10_23_152918_create_permissions_roles_table.php
+++ app/database/migrations/2015_10_23_152918_create_permissions_roles_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreatePermissionsRolesTable extends Migration {
+class CreatePermissionsrolesTable extends Migration {
 
 	/**
 	 * Run the migrations.
