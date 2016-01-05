05093b3ed696957dbb1047c48b0644387c66b9a4
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2016-01-05 18:25:09 -0300

    fix name

--- app/views/navs/left.blade.php
+++ app/views/navs/left.blade.php
@@ -21,7 +21,7 @@
                          <a href="/calendar/add">Agregar Hora</a>
                     </li>
                     <li>
-                        <a href="/therapist/calendar/add">Agregarme una Hora</a>
+                        <a href="/therapist/calendar/add">Agregar una Hora a Mi Calendario</a>
                     </li>
                     <li>
                         <a href="/my/calendar">Mi Calendario</a>
