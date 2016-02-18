8397141f12f840ab36f43a8dc46b8c3f98fc77ff
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2016-02-18 14:52:21 -0300

    fix admin options

--- app/views/admins/index.blade.php
+++ app/views/admins/index.blade.php
@@ -39,11 +39,11 @@
 
                                 <div class="btn-group" role="group" aria-label="...">
                                     <button type="button" access="0" therapist-id="{{$therapist->id}}"
-                                            class="btn btn-default @if($therapist->access == 0)active disabled @endif access">
+                                            class="btn btn-default @if($therapist->access == 0)active @endif access">
                                         NO
                                     </button>
                                     <button type="button" access="1" therapist-id="{{$therapist->id}}"
-                                            class="btn btn-default @if($therapist->access == 1)active disabled @endif access">
+                                            class="btn btn-default @if($therapist->access == 1)active  @endif access">
                                         SI
                                     </button>
                                 </div>
@@ -152,8 +152,8 @@
                 success: function (data) {
                 }
             });
-            $(this).parent().children("button").removeClass('disabled active');
-            $(this).addClass('disabled active')
+            $(this).parent().children("button").removeClass('active');
+            $(this).addClass('active')
         });
 
     </script>
